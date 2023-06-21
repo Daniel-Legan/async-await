e3e00881462e3bbd1a7d525140e789060f0b2b41
Daniel Legan <legan.daniel@gmail.com>
2023-06-21 17:27:12 -0400

    async-await example with api call

--- cra-async-await/src/App.js
+++ cra-async-await/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+const apiData = "https://fakestoreapi.com/products";
 
 function App() {
+  const [title, setTitle] = useState([]);
+  const [image, setImage] = useState([]);
+  const [description, setDescription] = useState([]);
+
+  useEffect(() => {
+    async function getStoreData() {
+      const response = await axios.get(`${apiData}/2`);
+      // console.log(response);
+      setTitle(response.data.title);
+      setImage(response.data.image);
+      console.log(response.data.image);
+      setDescription(response.data.description);
+    }
+    getStoreData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>{title}</h2>
+      <img src={image} alt=""/>
+      <p>{description}</p>
     </div>
   );
 }
